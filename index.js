1e35ab8687e6de4e30a20fc8e019090fa2fbba21
xebec19 <rohant946@gmail.com>
2022-04-09 16:44:45 +0530

    save

--- index.js
+++ index.js
@@ -1,33 +1,31 @@
-const cluster = require('cluster');
+process.env.UV_THREADPOOL_SIZE = 1;
+const cluster = require("cluster");
 
 // is the file is being executed in master mode ?
-if(cluster.isMaster){
-    // Cause index.js to be executed *again* but child mode
-    cluster.fork();
+if (cluster.isMaster) {
+  // Cause index.js to be executed *again* but child mode
+  cluster.fork();
+  cluster.fork();
 } else {
-    // I am a child, I am going to act like a server
-    const express = require('express');
-    const app = express();
-    const port = 3000;
+  // I am a child, I am going to act like a server
+  const express = require("express");
+  const crypto = require('crypto');
+  const app = express();
+  const port = 3000;
 
-    function doWork(duration) {
-        const start = Date.now();
-        while(Date.now() - start < duration){
-
-        }
-    }
-
-    app.get('/',(req,res) => {
-        doWork(5000);
-        res.send('Hi There');
+  app.get("/", (req, res) => {
+    crypto.pbkdf2("a", "b", 1000000, 512, "sha512", () => {
+    //   console.log("1:", Date.now() - start);
+      res.send("Hi There");
     });
 
-    app.get('/fast',(req,res) => {
-        res.send('Fast!!')
-    })
+  });
 
-    app.listen(port,() => {
-        console.log('Running on port '+ port);
-    });
+  app.get("/fast", (req, res) => {
+    res.send("Fast!!");
+  });
 
+  app.listen(port, () => {
+    console.log("Running on port " + port);
+  });
 }
